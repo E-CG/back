6dce330b86fc13e1035307271a057a5448389cfe
EstebanC <estebancogo47@gmail.com>
2023-10-23 23:13:08 -0500

    Arreglando clases

--- src/main/java/co/udea/ssmu/api/model/jpa/model/strategy/Strategy.java
+++ src/main/java/co/udea/ssmu/api/model/jpa/model/strategy/Strategy.java
@@ -19,7 +19,6 @@ public class Strategy {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "estrategia_seq")
     @SequenceGenerator(name = "estrategia_seq", sequenceName = "estrategia_seq", allocationSize = 1)
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_estrategia")
     private int idStrategy;
 
