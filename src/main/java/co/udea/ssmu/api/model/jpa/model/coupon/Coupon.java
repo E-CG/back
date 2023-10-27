ce75713a419ad4d77343d242f53bcda03b87a5be
EstebanC <estebancogo47@gmail.com>
2023-10-27 09:38:31 -0500

    Agregando lombok

--- src/main/java/co/udea/ssmu/api/model/jpa/model/coupon/Coupon.java
+++ src/main/java/co/udea/ssmu/api/model/jpa/model/coupon/Coupon.java
@@ -1,16 +1,16 @@
 package co.udea.ssmu.api.model.jpa.model.coupon;
 
 import co.udea.ssmu.api.model.jpa.model.strategy.Strategy;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
+import lombok.*;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
 @Entity
 @Table(name = "cupon")
 public class Coupon {
@@ -30,39 +30,4 @@ public class Coupon {
     @ManyToOne
     @JoinColumn(name = "id_estrategia", nullable = false)
     private Strategy strategy;
-
-    public Coupon() {
-    }
-
-    public int getAmount() {
-        return amount;
-    }
-
-    public void setAmount(int amount) {
-        this.amount = amount;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public Strategy getIdStrategy() {
-        return strategy;
-    }
-
-    public void setIdStrategy(Strategy strategy) {
-        this.strategy = strategy;
-    }
 }
--- src/main/java/co/udea/ssmu/api/model/jpa/model/strategy/Strategy.java
+++ src/main/java/co/udea/ssmu/api/model/jpa/model/strategy/Strategy.java
@@ -1,26 +1,26 @@
 package co.udea.ssmu.api.model.jpa.model.strategy;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.List;
 import co.udea.ssmu.api.model.jpa.model.coupon.Coupon;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.SequenceGenerator;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
+import lombok.*;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
 @Entity
 @Table(name = "estrategia")
-public class Strategy {
+public class Strategy implements Serializable {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "estrategia_seq")
-    @SequenceGenerator(name = "estrategia_seq", sequenceName = "estrategia_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id_estrategia")
-    private int idStrategy;
+    private long idStrategy;
 
     @NotNull
     @Column(name = "nombre")
@@ -62,105 +62,4 @@ public class Strategy {
 
     @OneToMany(mappedBy = "strategy")
     private List<Coupon> coupon;
-
-    public Strategy() {
-    }
-
-    public int getIdStrategy() {
-        return idStrategy;
-    }
-
-    public void setIdStrategy(int idStrategy) {
-        this.idStrategy = idStrategy;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public int getDiscountPercentage() {
-        return discountPercentage;
-
-    }
-
-    public void setDiscountPercentage(int discountPercentage) {
-        this.discountPercentage = discountPercentage;
-    }
-
-    public String getIsActive() {
-        return isActive;
-    }
-
-    public void setIsActive(String isActive) {
-        this.isActive = isActive;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public LocalDate getStartDate() {
-        return startDate;
-    }
-
-    public void setStartDate(LocalDate startDate) {
-        this.startDate = startDate;
-    }
-
-    public LocalDate getEndDate() {
-        return endDate;
-    }
-
-    public void setEndDate(LocalDate endDate) {
-        this.endDate = endDate;
-    }
-
-
-    public int getDiscountValue() {
-        return discountValue;
-    }
-
-    public void setDiscountValue(int discountValue) {
-        this.discountValue = discountValue;
-    }
-
-    public int getMinValue() {
-        return minValue;
-    }
-
-    public void setMinValue(int minValue) {
-        this.minValue = minValue;
-    }
-
-    public int getMaxDiscount() {
-        return maxDiscount;
-    }
-
-    public void setMaxDiscount(int maxDiscount) {
-        this.maxDiscount = maxDiscount;
-    }
-
-    public List<Coupon> getCoupons() {
-        return coupon;
-    }
-
-    public void setCoupons(List<Coupon> coupon) {
-        this.coupon = coupon;
-    }
 }
