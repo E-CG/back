38a0026350681f53fa2b872605402db7c5ee357a
EstebanC <estebancogo47@gmail.com>
2023-11-05 11:57:51 -0500

    Corrección propiedades de las columnas

--- src/main/java/co/udea/ssmu/api/model/jpa/model/Strategy.java
+++ src/main/java/co/udea/ssmu/api/model/jpa/model/Strategy.java
@@ -21,10 +21,10 @@ public class Strategy implements Serializable {
     private long idStrategy;
 
     @Column(name = "nombre", nullable = false, length = 50)
-    private @NonNull String name;
+    private String name;
 
     @Column(name = "descripcion", nullable = false, length = 50)
-    private @NonNull String description;
+    private String description;
 
     @Column(name = "porcentaje_descuento", nullable = false, length = 3)
     private int discountPercentage;
@@ -33,13 +33,13 @@ public class Strategy implements Serializable {
     private Boolean isActive;
 
     @Column(name = "ciudad", nullable = false, length = 50)
-    private @NonNull String city;
+    private String city;
 
     @Column(name = "fecha_inicio", nullable = false)
-    private @NonNull LocalDate startDate;
+    private LocalDate startDate;
 
     @Column(name = "fecha_fin", nullable = false)
-    private @NonNull LocalDate endDate;
+    private LocalDate endDate;
 
     @Column(name = "valor_descuento", nullable = true, length = 5)
     private int discountValue;
@@ -50,7 +50,6 @@ public class Strategy implements Serializable {
     @Column(name = "descuento_maximo", nullable = false, length = 5)
     private int maxDiscount;
 
-    //
     @ManyToMany(mappedBy = "strategies")
     private List<User> users;
 }
