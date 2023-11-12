5a6d96eb7ec081eac2933957190361c711874265
david.gil2 <david.gil2@udea.edu.co>
2023-11-12 13:44:43 -0500

    Arreglando Query

--- src/main/java/co/udea/ssmu/api/model/jpa/repository/strategy/IStrategyRepository.java
+++ src/main/java/co/udea/ssmu/api/model/jpa/repository/strategy/IStrategyRepository.java
@@ -7,11 +7,10 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import co.udea.ssmu.api.model.jpa.model.Coupon;
 import co.udea.ssmu.api.model.jpa.model.Strategy;
 
 @Repository
 public interface IStrategyRepository extends JpaRepository<Strategy, Long>{
     @Query(value = "SELECT s FROM Strategy s ORDER BY s.name DESC")
-    List<Coupon> findAllWithQueryParam(@Param("limit") int limit, @Param("offset") int offset);
+    List<Strategy> findAllWithQueryParam(@Param("limit") int limit, @Param("offset") int offset);
 }
